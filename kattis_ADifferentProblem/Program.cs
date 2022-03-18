767101e022a6ee5be915907f558991101c40f8e2
Leili <Leili@DESKTOP-JMUT38S>
2022-03-18 16:15:03 +0100

    Add project files.

---
+++ kattis_ADifferentProblem/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace kattis_ADifferentProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            while ((line = Console.ReadLine()) != null)
+            {
+                string[] split = line.Split(new char[] { ' ' }, StringSplitOptions.None);
+                long nr1 = Int64.Parse(split[0]);
+                long nr2 = Int64.Parse(split[1]);
+                Console.WriteLine(nr1 > nr2 ? (nr1 - nr2) : (nr2 - nr1));
+            }
+        }
+    }
+}
